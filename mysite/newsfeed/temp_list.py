12324ee5c77f2f42bcb89e4bb355bf1d4043eb03
Adnan Islam <islamadnan96@gmail.com>
2017-11-26 18:18:02 -0500

    created mock data to use while api remains to be implemented

---
+++ mysite/newsfeed/temp_list.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+articles = [
+    {
+        "source_id": "abc-news",
+        "title": "Terror threat to US rail puts law enforcement on alert",
+        "url": "http://abcnews.go.com/Politics/terror-threat-us-rail-puts-law-enforcement-alert/story?id=51204458"
+    },
+    {
+        "source_id": "al-jazeera-english",
+        "title": "Dozens of civilians killed in Syria air strikes",
+        "url": "http://www.aljazeera.com/news/2017/11/dozens-civilians-killed-syria-air-strikes-171126174857574.html"
+    },
+    {
+        "source_id": "ars-technica",
+        "title": "Fantastic Arcade celebrates weird gaming—dancing bears, haunted NESes, and all",
+        "url": "https://arstechnica.com/gaming/2017/11/fantastic-arcade-celebrates-weird-gaming-dancing-bears-haunted-neses-and-all/"
+    },
+    {
+        "source_id": "associated-press",
+        "title": "FBI gave heads-up to fraction of Russian hackers' US targets",
+        "url": "https://apnews.com/f1a5570b7ce04d39bab00ae3a9041460"
+    },
+    {
+        "source_id": "axios",
+        "title": "The week ahead for Trump and Congress",
+        "url": "https://www.axios.com/the-week-ahead-for-trump-and-congress-2512775824.html"
+    },
+    {
+        "source_id": "bbc-news",
+        "title": "Mugabe 'still has political role to play'",
+        "url": "http://www.bbc.co.uk/news/world-africa-42130757"
+    },
+    {
+        "source_id": "bleacher-report",
+        "title": "NFL Week 12: Sunday's Live Updates, Scores and Highlights",
+        "url": "http://bleacherreport.com/nfl-week-12-2017-11-26"
+    },
+    {
+        "source_id": "bloomberg",
+        "title": "Bitcoin Surges Past $9,000 as Euphoria Reaches a Fever Pitch",
+        "url": "http://www.bloomberg.com/news/articles/2017-11-26/bitcoin-surges-past-9-000-as-euphoria-reaches-a-fever-pitch"
+    },
+    {
+        "source_id": "breitbart-news",
+        "title": "Rep. John Conyers Steps Down from Judiciary Post amid Sexual Misconduct Controversy",
+        "url": "http://www.breitbart.com/big-government/2017/11/26/rep-john-conyers-steps-judiciary-post-sexual-misconduct-controversy/"
+    },
+    {
+        "source_id": "business-insider",
+        "title": "The CEO of the oldest airline in the world explains the major mistake the industry made 20 years ago",
+        "url": "http://www.businessinsider.com/klm-ceo-pieter-elbers-interview-2017-11",
+    }
+]
--- mysite/newsfeed/views.py
+++ mysite/newsfeed/views.py
@@ -6,8 +6,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 import services
+import temp_list 
 
 
 def index(request):
-    services.get_headlines()
+    #services.get_headlines()
+    print temp_list.articles
     return HttpResponse("Hello, world. You're at the newsfeed index.")
