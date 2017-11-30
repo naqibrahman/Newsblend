04289e0ac4e82e8b3f8f8c2348ae83f4c47cab00
Adnan Islam <islamadnan96@gmail.com>
2017-11-30 02:36:29 -0500

    created template to render article list

--- mysite/newsfeed/views.py
+++ mysite/newsfeed/views.py
@@ -5,11 +5,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 import services
 import temp_list 
 
 
 def index(request):
     #services.get_headlines()
-    print temp_list.articles
-    return HttpResponse("Hello, world. You're at the newsfeed index.")
+    article_list = temp_list.articles
+    context = {
+        'article_list': article_list,
+    }
+    return render(request, 'newsfeed/index.html', context)
