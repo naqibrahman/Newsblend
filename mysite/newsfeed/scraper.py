5e4ce4806faa500bd2ccddf3909f28ad2f68c1a4
naqibur rahman <nr1355@nyu.edu>
2017-12-02 23:08:31 -0500

    some error handling

--- mysite/newsfeed/scraper.py
+++ mysite/newsfeed/scraper.py
@@ -1,15 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 from temp_list import articles
+#TODO: Error Handling
 
 URLS = [ article["url"] for article in articles]
 def scrape(url):
-    mysession = requests.Session()
-    data = mysession.get(url).content
-    soup = BeautifulSoup(data)
-    ptags = soup.find_all('p');
-    data = [p.contents[0] for p in ptags]
-    data = [ text.replace('\n','') for text in data]
+    try:
+        mysession = requests.Session()
+        data = mysession.get(url).content
+        soup = BeautifulSoup(data)
+        ptags = soup.find_all('p');
+        data = [p.contents[0] for p in ptags]
+        data = [ text.replace('\n','') for text in data]
+    except:
+        data = ["There was an error retrieving this site"]
     return data
 
-
+def testrun():
+    return [ scrape(url) for url in URLS]
+print(URLS[1]);
+print(scrape(URLS[1]));
