ba0125fa79e225265aa7a6941b4bd98a8dc18a42
unknown <riosr03@gmail.com>
2017-12-03 23:31:58 -0500

    adds index page

--- mysite/newsfeed/urls.py
+++ mysite/newsfeed/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+	#url(r'^$', views.index, name='index'),
+    url(r'^$', views.newsfeed, name='newsfeed'),
 	url(r'^profile/$', views.profile, name='profile')
 ]
