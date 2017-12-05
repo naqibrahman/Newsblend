73d49b92e375e16297445d95906b55f015e5d483
naqibur rahman <nr1355@nyu.edu>
2017-12-04 20:13:49 -0500

    updated scraper and scraped text for demo

--- mysite/newsfeed/scraper.py
+++ mysite/newsfeed/scraper.py
@@ -8,15 +8,20 @@ def scrape(url):
     try:
         mysession = requests.Session()
         data = mysession.get(url).content
-        soup = BeautifulSoup(data)
+        soup = BeautifulSoup(data,"html.parser")
         ptags = soup.find_all('p');
-        data = [p.contents[0] for p in ptags]
+        data = [str(p) for p in ptags]
         data = [ text.replace('\n','') for text in data]
     except:
-        data = ["There was an error retrieving this site"]
+          data = ["There was an error retrieving this site"]
     return data
 
 def testrun():
-    return [ scrape(url) for url in URLS]
-print(URLS[1]);
-print(scrape(URLS[1]));
+    x = [ scrape(url) for url in URLS]
+    text = ["".join(y)+"\n\n ARTICLE END \n\n" for y in x ]
+    
+    print(text)
+    with open ('scrapings.text','w') as myf:
+        myf.writelines(text)
+#print(URLS[1])
+#print(testrun())
