81982a6a24645441c6c6e6590e9d48fd037d3d69
Adnan Islam <islamadnan96@gmail.com>
2017-12-05 11:50:33 -0500

    modified production settings; specified nullable fields in models

--- mysite/randomize_bias.bat
+++ mysite/randomize_bias.bat
@@ -1,3 +1,1 @@
-rem sudo -u postgres -H -- psql -d newsblend -c "UPDATE newsfeed_source SET score_average=(random()*11-5)"
-
 psql -U postgres -d newsblend -c "UPDATE newsfeed_source SET score_average=(random()*11-5)"
